f714d8bad277e119f994e64cffe9a9bd9f146dca
Andrés G <jose.garcia@cidecuc.mx>
2024-02-22 19:24:22 -0600

    Validacion de roles

--- routes/api.php
+++ routes/api.php
@@ -24,39 +24,89 @@ Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::get('user-profile', [AuthController::class, 'userProfile']);
     Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::prefix('v1/university')-> group(function() {
+
+        Route::group(['middleware' => ['auth:sanctum', 'role:2']], function() {
+            Route::prefix('alumno')-> group(function() {
+                Route::get('/kardex-pdf/{id_alumno}', [KardexController::class, 'pdfKardex']);
+                Route::get('/kardex/{id_alumno}', [KardexController::class, 'show']);
+            });
+            
+        });
+
+        Route::group(['middleware' => ['auth:sanctum', 'role:1']], function() {
+            Route::prefix('admin') -> controller(UserController::class) -> group(function() {
+                Route::get('/admins', 'index');
+                Route::get('/admin/{id_user}', 'show');
+                Route::post('/register', 'store');
+                Route::patch('/update/{id_user}', 'update');
+            });
+
+            Route::prefix('alumno') -> controller(AlumnoController::class) -> group(function() {
+                Route::get('/alumnos', 'index');
+                Route::get('/alumno/{id_alumno}', 'show');
+                Route::post('/register', 'storeAlumno');
+                Route::patch('/update/{id_alumno}', 'update');
+            });
+
+                
+            Route::prefix('materia') -> controller(MateriaController::class) -> group(function() {
+                Route::get('/materias', 'index');
+                Route::get('/materia/{id_materia}', 'show');
+                Route::post('/register', 'store');
+                Route::patch('/update/{id_materia}', 'update');
+            });
+
+            Route::prefix('kardex') -> controller(KardexController::class) -> group(function() {
+                Route::post('/register', 'store');
+                Route::put('/calificar/{id_alumno}', 'update');
+                Route::get('/kardex/{id_alumno}', 'show');
+            }); 
+
+        });
+    
+
+    
+
+    
+        Route::put('/restore', [UserController::class, 'restorePassword']);
+        
+    
+    });
 });
 
-Route::prefix('v1/university')-> group(function() {
+// Route::prefix('v1/university')-> group(function() {
 
-    //MANAGER
-    Route::prefix('admin') -> controller(UserController::class) -> group(function() {
-        Route::get('/admins', 'index');
-        Route::get('/admin/{id_user}', 'show');
-        Route::post('/register', 'store');
-        Route::patch('/update/{id_user}', 'update');
-    });
+//     //MANAGER
+//     Route::prefix('admin') -> controller(UserController::class) -> group(function() {
+//         Route::get('/admins', 'index');
+//         Route::get('/admin/{id_user}', 'show');
+//         Route::post('/register', 'store');
+//         Route::patch('/update/{id_user}', 'update');
+//     });
 
-    Route::prefix('alumno') -> controller(AlumnoController::class) -> group(function() {
-        Route::get('/alumnos', 'index');
-        Route::get('/alumno/{id_alumno}', 'show');
-        Route::post('/register', 'storeAlumno');
-        Route::patch('/update/{id_alumno}', 'update');
-    });
+//     Route::prefix('alumno') -> controller(AlumnoController::class) -> group(function() {
+//         Route::get('/alumnos', 'index');
+//         Route::get('/alumno/{id_alumno}', 'show');
+//         Route::post('/register', 'storeAlumno');
+//         Route::patch('/update/{id_alumno}', 'update');
+//     });
 
-    Route::prefix('materia') -> controller(MateriaController::class) -> group(function() {
-        Route::get('/materias', 'index');
-        Route::get('/materia/{id_materia}', 'show');
-        Route::post('/register', 'store');
-        Route::patch('/update/{id_materia}', 'update');
-    });
+//     Route::prefix('materia') -> controller(MateriaController::class) -> group(function() {
+//         Route::get('/materias', 'index');
+//         Route::get('/materia/{id_materia}', 'show');
+//         Route::post('/register', 'store');
+//         Route::patch('/update/{id_materia}', 'update');
+//     });
 
-    Route::prefix('kardex') -> controller(KardexController::class) -> group(function() {
-        Route::post('/register', 'store');
-        Route::put('/calificar/{id_alumno}', 'update');
-        Route::get('/kardex/{id_alumno}', 'show');
-    }); 
+//     Route::prefix('kardex') -> controller(KardexController::class) -> group(function() {
+//         Route::post('/register', 'store');
+//         Route::put('/calificar/{id_alumno}', 'update');
+//         Route::get('/kardex/{id_alumno}', 'show');
+//     }); 
 
-    Route::put('/restore', [UserController::class, 'restorePassword']);
-    Route::get('/kardex-pdf/{id_alumno}', [KardexController::class, 'pdfKardex']);
+//     Route::put('/restore', [UserController::class, 'restorePassword']);
+//     Route::get('/kardex-pdf/{id_alumno}', [KardexController::class, 'pdfKardex']);
 
-});
+// });
